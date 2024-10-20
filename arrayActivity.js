b74b6dc234f5ad76986c21363b71a80f64c52a7c
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-10-20 08:02:45 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+const favSinger  = ["Ice Cube, 2 Pac, Playboy Carti"];
+console.log(favSinger[2]);
+
+const FavNumbers = ["12, 14, 4, 36"];
+
+const mixedArr = ["string", ["onotherarray"], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
